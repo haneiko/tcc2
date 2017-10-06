8970cb38910a88fb629b516d64868eacb0683973
Mateus C. Haneiko <mateus.haneiko@gmail.com>
2017-10-06 15:57:39 -0300

    removed coments on mtime.c

--- mtime.c
+++ mtime.c
@@ -5,13 +5,8 @@ int main()
 {
 	struct timeval t1;
 	double time;
-
 	gettimeofday(&t1, NULL);
-
-	/* us to ms */
-	/* time = (t1.tv_sec * 1000.0) + (t1.tv_usec / 1000.0); */
 	time = t1.tv_sec + (t1.tv_usec / 1000000.0);
 	printf("%f", time);
-
 	return 0;
 }
